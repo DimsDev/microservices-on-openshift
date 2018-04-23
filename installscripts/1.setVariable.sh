eb827e746ecfbe87745e7ecd577311774ea0309d
dimsdev <dimsdev@gmail.com>
2018-04-23 15:47:25 +0200

    Modif ..

--- installscripts/1.setVariable.sh
+++ installscripts/1.setVariable.sh
@@ -1,12 +1,13 @@
-export OSE_DOMAIN=apps.devday.ocpcloud.com
+#!/usr/bin/env bash
+export OSE_DOMAIN=apps.172.20.12.98:8443
 export OSE_CLIENT_PROJECT=msservices
 export OSE_SERVICES_PROJECT=msservices
 export OSE_INFRA_PROJECT=msservices
-export FROM_GMAIL=<<from email for your email service>>
-export FROM_GMAIL_PASSWORD=<<password for the from email>>
-//For these params refer https://dev.twitter.com/oauth/overview/application-owner-access-tokens
-export TWITTER_CONSUMER_KEY=<<Consumer Key (API Key)>>
-export TWITTER_CONSUMER_SECRET=<<Consumer Secret (API Secret)>>
-export TWITTER_OAUTH_ACCESS_TOKEN=<<Access Token>>
-export TWITTER_OAUTH_ACCESS_TOKEN_SECRET=<<Access Token Secret>>
+export FROM_GMAIL=restozed@gmail.com
+export FROM_GMAIL_PASSWORD=bcbg2k14
+#//For these params refer https://dev.twitter.com/oauth/overview/application-owner-access-tokens
+#export TWITTER_CONSUMER_KEY=<<Consumer Key (API Key)>>
+#export TWITTER_CONSUMER_SECRET=<<Consumer Secret (API Secret)>>
+#export TWITTER_OAUTH_ACCESS_TOKEN=<<Access Token>>
+#export TWITTER_OAUTH_ACCESS_TOKEN_SECRET=<<Access Token Secret>>
 
