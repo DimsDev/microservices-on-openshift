afc3b8e5dfc1afbabcab1b8108d208d693a08489
DimsDev <37579919+DimsDev@users.noreply.github.com>
2018-04-24 09:27:14 +0200

    ---

--- installscripts/1.setVariable.sh
+++ installscripts/1.setVariable.sh
@@ -3,8 +3,8 @@ export OSE_DOMAIN=apps.172.20.12.98:8443
 export OSE_CLIENT_PROJECT=msservices
 export OSE_SERVICES_PROJECT=msservices
 export OSE_INFRA_PROJECT=msservices
-export FROM_GMAIL=restozed@gmail.com
-export FROM_GMAIL_PASSWORD=bcbg2k14
+export FROM_GMAIL=adress@gmail.com
+export FROM_GMAIL_PASSWORD=pswrd
 #//For these params refer https://dev.twitter.com/oauth/overview/application-owner-access-tokens
 #export TWITTER_CONSUMER_KEY=<<Consumer Key (API Key)>>
 #export TWITTER_CONSUMER_SECRET=<<Consumer Secret (API Secret)>>
